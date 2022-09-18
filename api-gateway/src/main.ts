e81753562ee51e3e96c13f4e078140f80ecd365e
Felipe <felipeoliveirasilva12@outlook.com>
2022-09-17 21:58:45 -0300

    feat(API-GATEWAY): exception filter

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import momentTimezone from 'moment-timezone';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
 import { TimeoutInterceptor } from './interceptor/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(new TimeoutInterceptor());
+  app.useGlobalInterceptors(new TimeoutInterceptor());
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
 
   await app.listen(3000);
 }
